afe0d4457efde56a0899e458a2b99ee3450de91c
Akshay Malshetwar <akshaymalshetwar@gmail.com>
2024-05-31 15:20:47 +0530

    commit by sujata in branch

--- LMS-EmployeeService/src/main/java/com/lms/employeeservice/employeecontroller/EmployeeController.java
+++ LMS-EmployeeService/src/main/java/com/lms/employeeservice/employeecontroller/EmployeeController.java
@@ -17,7 +17,7 @@ public class EmployeeController {
 	@PostMapping("/employee")
 	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee emp){
 		Employee employee = esi.saveEmployee(emp);
-		System.out.println("Git practice");
+		System.out.println("Git practice by sujata");
 		return new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
 	}
 }
