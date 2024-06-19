7ec107ade057755ce3479c16c6d3d19442d53107
RakhiKhadse <rakhikhadse009@gmail.com>
2024-06-19 17:30:52 +0530

    Load disbursement class fields changed by rakhi

--- LMS-CustomerService1/src/main/java/com/lms/customerservice/app/model/LoanDisbursement.java
+++ LMS-CustomerService1/src/main/java/com/lms/customerservice/app/model/LoanDisbursement.java
@@ -1,5 +1,7 @@
 package com.lms.customerservice.app.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,12 +19,15 @@ public class LoanDisbursement {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer agreementId;
+	private Date agreementDate;
 	private Double totalLoanSanctionedAmount;
 	private Double transferedAmount;
 	private String amountPaidDate;
 	private String paymentStatus;
+	private String bankName;
 	private Long bankAccountNumber;
-	private String bankIfscNumber;
+	private String bankIfscCode;
+	private String accountType;
 
 
 }
